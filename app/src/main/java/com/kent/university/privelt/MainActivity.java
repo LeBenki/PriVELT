099ca37a54412cdf912982facbd2fe1e10d1f06a
Lucas Benkemoun <lucas99.06@gmail.com>
2019-12-18 14:47:17 +0100

    Initial project with web scrapper dependence

---
+++ app/src/main/java/com/kent/university/privelt/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kent.university.privelt;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
