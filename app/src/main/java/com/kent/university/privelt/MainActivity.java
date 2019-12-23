b032a1f7a6f446597ea835d17d0bce33bdf481f3
Lucas Benkemoun <lucas99.06@gmail.com>
2019-12-23 17:52:41 +0100

    Updated strava and icons

--- app/src/main/java/com/kent/university/privelt/MainActivity.java
+++ app/src/main/java/com/kent/university/privelt/MainActivity.java
@@ -24,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setItemIconTintList(null);
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
