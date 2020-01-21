311550f050f0d9137fc4675e2bda24b566e4fee7
Lucas Benkemoun <lucas99.06@gmail.com>
2020-01-21 15:27:02 +0000

    First implementation of radar chart

---
+++ app/src/main/java/com/kent/university/privelt/ui/dashboard/risk_value/RadarMarkerView.java
@@ -0,0 +1,39 @@
+package com.kent.university.privelt.ui.dashboard.risk_value;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.utils.MPPointF;
+import com.kent.university.privelt.R;
+
+import java.text.DecimalFormat;
+
+public class RadarMarkerView extends MarkerView {
+
+    private final TextView tvContent;
+    private final DecimalFormat format = new DecimalFormat("##0");
+
+    public RadarMarkerView(Context context, int layoutResource) {
+        super(context, layoutResource);
+
+        tvContent = findViewById(R.id.tvContent);
+    }
+
+    // runs every time the MarkerView is redrawn, can be used to update the
+    // content (user-interface)
+    @Override
+    public void refreshContent(Entry e, Highlight highlight) {
+        tvContent.setText(String.format("%s %%", format.format(e.getY())));
+
+        super.refreshContent(e, highlight);
+    }
+
+    @Override
+    public MPPointF getOffset() {
+        return new MPPointF(-(getWidth() / 2), -getHeight() - 10);
+    }
+}
--- app/src/main/java/com/kent/university/privelt/ui/dashboard/risk_value/RiskValueFragment.java
+++ app/src/main/java/com/kent/university/privelt/ui/dashboard/risk_value/RiskValueFragment.java
@@ -1,6 +1,161 @@
 package com.kent.university.privelt.ui.dashboard.risk_value;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IRadarDataSet;
+import com.kent.university.privelt.R;
 import com.kent.university.privelt.base.BaseFragment;
 
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class RiskValueFragment extends BaseFragment {
+
+    @BindView(R.id.chart)
+    RadarChart chart;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_risk_value, container, false);
+        ButterKnife.bind(this, view);
+
+        configureChart();
+        return view;
+    }
+
+    private void configureChart() {
+//        chart.setBackgroundColor(Color.rgb(60, 65, 82));
+
+        chart.getDescription().setEnabled(false);
+
+        chart.setWebLineWidth(1f);
+        chart.setWebColor(Color.LTGRAY);
+        chart.setWebLineWidthInner(1f);
+        chart.setWebColorInner(Color.LTGRAY);
+        chart.setWebAlpha(100);
+
+        // create a custom MarkerView (extend MarkerView) and specify the layout
+        // to use for it
+        MarkerView mv = new RadarMarkerView(getContext(), R.layout.radar_markerview);
+        mv.setChartView(chart); // For bounds control
+        chart.setMarker(mv); // Set the marker to the chart
+
+        setData();
+
+        chart.animateXY(1400, 1400, Easing.EaseInOutQuad);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setTextSize(9f);
+        xAxis.setYOffset(0f);
+        xAxis.setXOffset(0f);
+        xAxis.setValueFormatter(new ValueFormatter() {
+
+            private final String[] mActivities = new String[]{"Devices", "Addresses", "Personal information", "Location History", "Planes history"};
+
+            @Override
+            public String getFormattedValue(float value) {
+                return mActivities[(int) value % mActivities.length];
+            }
+        });
+        //xAxis.setTextColor(Color.WHITE);
+
+        YAxis yAxis = chart.getYAxis();
+        yAxis.setLabelCount(5, false);
+        yAxis.setTextSize(9f);
+        yAxis.setAxisMinimum(0f);
+        yAxis.setAxisMaximum(80f);
+        yAxis.setDrawLabels(false);
+
+        Legend l = chart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(false);
+        l.setXEntrySpace(7f);
+        l.setYEntrySpace(5f);
+        //l.setTextColor(Color.WHITE);
+    }
+
+    private void setData() {
+
+        float mul = 80;
+        float min = 20;
+        int cnt = 5;
+
+        ArrayList<RadarEntry> entries1 = new ArrayList<>();
+        ArrayList<RadarEntry> entries2 = new ArrayList<>();
+        ArrayList<RadarEntry> entries3 = new ArrayList<>();
+
+        // NOTE: The order of the entries when being added to the entries array determines their position around the center of
+        // the chart.
+        for (int i = 0; i < cnt; i++) {
+            float val1 = (float) (Math.random() * mul) + min;
+            entries1.add(new RadarEntry(val1));
+
+            float val2 = (float) (Math.random() * mul) + min;
+            entries2.add(new RadarEntry(val2));
+
+            float val3 = (float) (Math.random() * mul) + min;
+            entries3.add(new RadarEntry(val3));
+        }
+
+        RadarDataSet set1 = new RadarDataSet(entries1, "Google");
+        set1.setColor(Color.YELLOW);
+        set1.setFillColor(Color.YELLOW);
+        set1.setDrawFilled(true);
+        set1.setFillAlpha(180);
+        set1.setLineWidth(2f);
+        set1.setDrawHighlightCircleEnabled(true);
+        set1.setDrawHighlightIndicators(false);
+
+        RadarDataSet set2 = new RadarDataSet(entries2, "Strava");
+        set2.setColor(Color.BLUE);
+        set2.setFillColor(Color.BLUE);
+        set2.setDrawFilled(true);
+        set2.setFillAlpha(180);
+        set2.setLineWidth(2f);
+        set2.setDrawHighlightCircleEnabled(true);
+        set2.setDrawHighlightIndicators(false);
+
+        RadarDataSet set3 = new RadarDataSet(entries3, "Hotels.com");
+        set3.setColor(Color.GREEN);
+        set3.setFillColor(Color.GREEN);
+        set3.setDrawFilled(true);
+        set3.setFillAlpha(180);
+        set3.setLineWidth(2f);
+        set3.setDrawHighlightCircleEnabled(true);
+        set3.setDrawHighlightIndicators(false);
+
+        ArrayList<IRadarDataSet> sets = new ArrayList<>();
+        sets.add(set1);
+        sets.add(set2);
+        sets.add(set3);
+
+        RadarData data = new RadarData(sets);
+        data.setValueTextSize(8f);
+        data.setDrawValues(false);
+        data.setValueTextColor(Color.WHITE);
+
+        chart.setData(data);
+        chart.invalidate();
+    }
 }
