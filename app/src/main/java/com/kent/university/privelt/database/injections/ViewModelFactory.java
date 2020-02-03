c121ea2f5a1e617b8d5e3799511fe9ecfcdb1fd4
Lucas Benkemoun <lucas99.06@gmail.com>
2020-02-03 21:01:47 +0000

    Removed unnecessary viewmodel

--- app/src/main/java/com/kent/university/privelt/database/injections/ViewModelFactory.java
+++ app/src/main/java/com/kent/university/privelt/database/injections/ViewModelFactory.java
@@ -7,7 +7,6 @@ import com.kent.university.privelt.ui.dashboard.risk_value.RiskValueViewModel;
 import com.kent.university.privelt.ui.dashboard.service.ServiceViewModel;
 import com.kent.university.privelt.ui.dashboard.user.UserViewModel;
 import com.kent.university.privelt.ui.data.DataViewModel;
-import com.kent.university.privelt.ui.master_password.MasterPasswordViewModel;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.Executor;
@@ -33,14 +32,7 @@ public class ViewModelFactory implements ViewModelProvider.Factory {
     @Override
     public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
         try {
-            if (MasterPasswordViewModel.class.equals(modelClass)) {
-                return modelClass.getConstructor(UserDataRepository.class,
-                        ServiceDataRepository.class,
-                        CurrentUserDataRepository.class,
-                        Executor.class)
-                        .newInstance(mUserDataSource, mServiceDataSource, mCurrentUserDataRepository, mExecutor);
-            }
-            else if (ServiceViewModel.class.equals(modelClass)) {
+            if (ServiceViewModel.class.equals(modelClass)) {
                 return modelClass.getConstructor(ServiceDataRepository.class,
                         UserDataRepository.class,
                         Executor.class)
