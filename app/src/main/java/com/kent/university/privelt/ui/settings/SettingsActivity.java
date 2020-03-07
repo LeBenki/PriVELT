b96f862dc759342d5fcda30e7a99859f0ca7462e
Lucas Benkemoun <lucas99.06@gmail.com>
2020-03-07 17:07:18 +0000

    Implemented Google Drive

--- app/src/main/java/com/kent/university/privelt/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/kent/university/privelt/ui/settings/SettingsActivity.java
@@ -1,12 +1,30 @@
 package com.kent.university.privelt.ui.settings;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.Scope;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.http.ByteArrayContent;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
 import com.kent.university.privelt.R;
+import com.kent.university.privelt.database.PriVELTDatabase;
 import com.kent.university.privelt.ui.master_password.MasterPasswordActivity;
+import com.kent.university.privelt.utils.DriveServiceHelper;
+
+import java.util.Collections;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +33,9 @@ import butterknife.ButterKnife;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE_SIGN_IN = 1;
+    private static final int REQUEST_CODE_OPEN_DOCUMENT = 2;
+
     public static final String ARG_CHANGE_PASSWORD = "ARG_CHANGE_PASSWORD";
 
     @BindView(R.id.change_password)
@@ -22,6 +43,12 @@ public class SettingsActivity extends AppCompatActivity {
 
     @BindView(R.id.logout)
     Button logout;
+
+    @BindView(R.id.drive)
+    Button drive;
+
+    private DriveServiceHelper mDriveServiceHelper;
+    private String mOpenFileId;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,6 +73,145 @@ public class SettingsActivity extends AppCompatActivity {
                 })
                 .setNegativeButton(R.string.no, null)
                 .show());
+
+        drive.setOnClickListener(view -> {
+            GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(SettingsActivity.this);
+
+            if (account == null) {
+                requestSignIn();
+            } else {
+
+                GoogleAccountCredential credential =
+                        GoogleAccountCredential.usingOAuth2(
+                                this, Collections.singleton(DriveScopes.DRIVE_FILE));
+                credential.setSelectedAccount(account.getAccount());
+                com.google.api.services.drive.Drive googleDriveService =
+                        new com.google.api.services.drive.Drive.Builder(
+                                AndroidHttp.newCompatibleTransport(),
+                                new GsonFactory(),
+                                credential)
+                                .setApplicationName("PriVELT")
+                                .build();
+                mDriveServiceHelper = new DriveServiceHelper(googleDriveService);
+                saveFile();
+            }
+        });
     }
 
+    private void requestSignIn() {
+
+        GoogleSignInOptions signInOptions =
+                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestEmail()
+                        .requestScopes(new Scope(DriveScopes.DRIVE_FILE))
+                        .build();
+        GoogleSignInClient client = GoogleSignIn.getClient(this, signInOptions);
+
+        // The result of the sign-in Intent is handled in onActivityResult.
+        startActivityForResult(client.getSignInIntent(), REQUEST_CODE_SIGN_IN);
+    }
+
+    /**
+     * Opens the Storage Access Framework file picker using {@link #REQUEST_CODE_OPEN_DOCUMENT}.
+     */
+    private void openFilePicker() {
+        if (mDriveServiceHelper != null) {
+
+            Intent pickerIntent = mDriveServiceHelper.createFilePickerIntent();
+
+            // The result of the SAF Intent is handled in onActivityResult.
+            startActivityForResult(pickerIntent, REQUEST_CODE_OPEN_DOCUMENT);
+        }
+    }
+
+    /**
+     */
+    private void saveFile() {
+        Log.d("LUCAS", "saveFile");
+
+            mDriveServiceHelper.uploadFile(getDatabasePath(PriVELTDatabase.PriVELTDatabaseName), null)
+                    .addOnFailureListener(exception -> { Log.d("LUCAS", "erreur merde sa mere" + exception.getMessage());})
+                    .addOnSuccessListener(runnable -> {Log.d("LUCAS", "bravo");});
+    }
+
+    /**
+     * Handles the {@code result} of a completed sign-in activity initiated from {@link
+     * #requestSignIn()}.
+     */
+    private void handleSignInResult(Intent result) {
+        GoogleSignIn.getSignedInAccountFromIntent(result)
+                .addOnSuccessListener(googleAccount -> {
+                    Log.d("TAG", "Signed in as " + googleAccount.getEmail());
+
+                    // Use the authenticated account to sign in to the Drive service.
+                    GoogleAccountCredential credential =
+                            GoogleAccountCredential.usingOAuth2(
+                                    this, Collections.singleton(DriveScopes.DRIVE_FILE));
+                    credential.setSelectedAccount(googleAccount.getAccount());
+                    Drive googleDriveService =
+                            new Drive.Builder(
+                                    AndroidHttp.newCompatibleTransport(),
+                                    new GsonFactory(),
+                                    credential)
+                                    .setApplicationName("Drive API Migration")
+                                    .build();
+
+                    // The DriveServiceHelper encapsulates all REST API and SAF functionality.
+                    // Its instantiation is required before handling any onClick actions.
+                    mDriveServiceHelper = new DriveServiceHelper(googleDriveService);
+                    saveFile();
+                })
+                .addOnFailureListener(exception -> Log.e("TAG", "Unable to sign in.", exception));
+    }
+
+    /**
+     * Opens a file from its {@code uri} returned from the Storage Access Framework file picker
+     * initiated by {@link #openFilePicker()}.
+     */
+    private void openFileFromFilePicker(Uri uri) {
+        if (mDriveServiceHelper != null) {
+
+            mDriveServiceHelper.openFileUsingStorageAccessFramework(getContentResolver(), uri)
+                    .addOnSuccessListener(nameAndContent -> {
+                        String name = nameAndContent.first;
+                        String content = nameAndContent.second;
+
+                        // Files opened through SAF cannot be modified.
+                        setReadOnlyMode();
+                    })
+                    .addOnFailureListener(exception ->
+                            Log.e("TAG", "Unable to open file from picker.", exception));
+        }
+    }
+
+    /**
+     * Updates the UI to read-only mode.
+     */
+    private void setReadOnlyMode() {
+    /*    mFileTitleEditText.setEnabled(false);
+        mDocContentEditText.setEnabled(false)*/;
+        mOpenFileId = null;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        switch (requestCode) {
+            case REQUEST_CODE_SIGN_IN:
+                if (resultCode == Activity.RESULT_OK && resultData != null) {
+                    handleSignInResult(resultData);
+                }
+                break;
+
+            case REQUEST_CODE_OPEN_DOCUMENT:
+                if (resultCode == Activity.RESULT_OK && resultData != null) {
+                    Uri uri = resultData.getData();
+                    if (uri != null) {
+                        openFileFromFilePicker(uri);
+                    }
+                }
+                break;
+        }
+
+        super.onActivityResult(requestCode, resultCode, resultData);
+    }
 }
