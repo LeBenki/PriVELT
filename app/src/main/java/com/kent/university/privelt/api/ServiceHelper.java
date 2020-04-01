2393a4fbdae1dced98dee13f8a3b99086c969543
Lucas Benkemoun <lucas99.06@gmail.com>
2020-04-01 15:04:13 +0200

    Added MPL2 license header

--- app/src/main/java/com/kent/university/privelt/api/ServiceHelper.java
+++ app/src/main/java/com/kent/university/privelt/api/ServiceHelper.java
@@ -1,7 +1,12 @@
+/*
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  * License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package com.kent.university.privelt.api;
 
 import android.content.Context;
-import android.util.Log;
 
 import com.kent.university.privelt.model.Service;
 import com.kent.university.webviewautologin.services.LoginService;
--- app/src/main/java/com/kent/university/privelt/model/Service.java
+++ app/src/main/java/com/kent/university/privelt/model/Service.java
@@ -1,9 +1,12 @@
+/*
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  * License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package com.kent.university.privelt.model;
 
-import com.google.api.services.drive.model.User;
-
 import java.io.Serializable;
-import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.room.Entity;
